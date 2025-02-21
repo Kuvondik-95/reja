46fde62fb7c1e5c8d2afb73b0a01a3fd48685152
Kuvondik-95 <kuvondik9595@gmail.com>
2025-02-21 18:56:39 +0900

    Hamma narsa tayyor faqat main.css ishlamayapti

--- server.js
+++ server.js
@@ -1,31 +1,39 @@
+// EJS bizga backend da frontendni qurib olis uchun yordam beradi. 
+
+
 console.log('Web serverni boshlash');
-const express = require('express');
-const app = express();
+const express = require('express'); // Express bizga backend serverni qurib beradi faqat.
+const app = express(); // bu yerda express bizga object return qiladi.
 const http = require('http');
 
-
 // 1 Intro codes
-app.use(express.static("public")); // public folderni clientlarga ochib berish(html, css va images)
-app.use(express.json()); // brauzerdan kelayotgan json formatdagi ma'lumotni object ko'rinishiga o'tqazib beradi
-app.use(express.urlencoded({extended: true})); // form dan post bo'lib kelgan narsalarni expressda qabul qilish uchun yoziladi
+app.use(express.static("public"));  // public folderni clientlarga ochib berish(html, css va images)
+app.use(express.json());            // REST API. brauzerdan kelayotgan json formatdagi ma'lumotni object ko'rinishiga o'tqazib beradi
+app.use(express.urlencoded({extended: true})); // TRADITIONAL API. form dan post bo'lib kelgan narsalarni expressda qabul qilish uchun yoziladi
 
 
 // 2 Session codes
 
 // 3 Views code          BSSR(backend server-side rendering)-Bakendda frontendni render qilib olish ya'ni serverda html ni qurib olib uni clientga yuborish. Bu usul traditional usul hisoblanad. 
-// VA Zamonaviy usul single page application usuli Buni Reactda qilamiz;
+// VA Zamonaviy usul SPA(single page application) usuli Buni Reactda qilamiz;
 app.set("views", "views");
 app.set("view engine", "ejs");
 
 
 // 4 Routing codes
+
+app.get("/harid", function(req, res){
+  res.render("harid");
+})
+
+
 app.post("/create-item", function(req, res){
   console.log(req.body);
   res.json({test:"success"});
 })
 
 app.get("/", function(req, res){
-    res.render("harid");
+    res.render("author");
 })
 
 
