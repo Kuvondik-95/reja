0ffc10e97941fe6b5939dd479170b9abaf3fcfa9
Kuvondik-95 <kuvondik9595@gmail.com>
2025-02-23 15:13:51 +0900

    BRR: train.js ichida A-TASK ishlandi

--- train.js
+++ train.js
@@ -2,7 +2,7 @@ console.log('Train task is running on!')
 // const cpus = require("os"); //Computer yadrosi nechta ekanlikini bilib olsa bo'ladi.
 // console.log(cpus); 
 
-console.log('Jack Ma maslahatlari!');
+// console.log('Jack Ma maslahatlari!');
 const list = [
   "Yaxshi talaba bo\'ling!", // 0-20,
   "to\'g\'ri boshliq tanlang va ko\'proq xato qiling!", // 20-30,
@@ -38,27 +38,27 @@ const list = [
 
 
 // Endi ASYNC function larni ko'rib chiqamiz
-async function maslahatBering(a){
-  if(typeof a !== "number") throw new Error("insert a number");
-  else if (a <= 0) return list[0];
-  else if(a > 20 && a <= 30) return list[1];
-  else if(a > 30 && a <= 40) return list[2];
-  else if(a > 40 && a <= 50) return list[3];
-  else if(a > 50 && a <= 60) return list[4];
-  else{
-    return new Promise((resolve, reject) =>{
-      setTimeout(()=>{
-        resolve(list[5]); // Bu yerda resolve return bilan bir xil ish qiladi.
-      }, 3000);
-    })
-    // return list[5];
-    // setTimeout(()=>{
-    //   callback(null, list[5]);
-    // }, 5000)
+// async function maslahatBering(a){
+//   if(typeof a !== "number") throw new Error("insert a number");
+//   else if (a <= 0) return list[0];
+//   else if(a > 20 && a <= 30) return list[1];
+//   else if(a > 30 && a <= 40) return list[2];
+//   else if(a > 40 && a <= 50) return list[3];
+//   else if(a > 50 && a <= 60) return list[4];
+//   else{
+//     return new Promise((resolve, reject) =>{
+//       setTimeout(()=>{
+//         resolve(list[5]); // Bu yerda resolve return bilan bir xil ish qiladi.
+//       }, 3000);
+//     })
+//     // return list[5];
+//     // setTimeout(()=>{
+//     //   callback(null, list[5]);
+//     // }, 5000)
     
-  }
-}
-console.log('passed here 0');
+//   }
+// }
+// console.log('passed here 0');
 
 //  call via then and catch
 // maslahatBering(20)
@@ -70,15 +70,45 @@ console.log('passed here 0');
 // })
 
 // call via ASYNC AWAIT
-async function run(){
-  let javob = await maslahatBering(70);
-  console.log('javob: ', javob);
-  // javob = await maslahatBering(30);
-  // console.log('javob: ', javob);
-  // javob = await maslahatBering(41);
-  // console.log('javob: ', javob);
+// async function run(){
+//   let javob = await maslahatBering(70);
+//   console.log('javob: ', javob);
+//   // javob = await maslahatBering(30);
+//   // console.log('javob: ', javob);
+//   // javob = await maslahatBering(41);
+//   // console.log('javob: ', javob);
+// }
+// run();
+
+// console.log('passed here 1');
+
+
+
+// A-TASK MIT24 
+function countLetter(word, letter ){
+  let summ = 0;
+  if(typeof word !== 'string' && typeof letter !== 'string'){
+    console.log("Please, insert a string!")
+  }else{
+    const myWord = word.split("");
+    for(let i=0; i < myWord.length; i++){
+      if(myWord[i] === letter){
+        summ ++;
+      }
+    }
+  }
+   console.log(summ);
 }
-run();
 
-console.log('passed here 1');
+countLetter('mashina', 'a');
 
+// run("a", "alabay", (err, data) => {
+//   if(err) console.log('ERROR:', err);
+//   else{
+//     console.log(data);
+//   }
+// })
+
+// run();
+// if(typeof letter !== "string" &&) callback("insert a number", null);
+// else if (typeof word !== "string") callback("insert a number", null);
