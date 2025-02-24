286428d8e50ef48325a92750c252847eb1107327
Kuvondik-95 <kuvondik9595@gmail.com>
2025-02-24 19:46:41 +0900

    feat: MongoDB ga ulanish

---
+++ app.js
@@ -0,0 +1,63 @@
+// EJS bizga backend da frontendni qurib olis uchun yordam beradi. 
+
+
+console.log('Web serverni boshlash');
+const express = require('express'); // Express bizga backend serverni qurib beradi faqat.
+const app = express(); // bu yerda express bizga object return qiladi.
+const http = require('http');
+
+
+
+// MongoDB ni chaqirish
+const db = require("./server").db();
+
+
+
+// 1 Intro codes
+// app.use(express.static("public"));  // public folderni clientlarga ochib berish(html, css va images)
+app.use(express.static('./public'));
+app.use(express.json());            // REST API. brauzerdan kelayotgan json formatdagi ma'lumotni object ko'rinishiga o'tqazib beradi
+app.use(express.urlencoded({extended: true})); // TRADITIONAL API. form dan post bo'lib kelgan narsalarni expressda qabul qilish uchun yoziladi
+
+
+// 2 Session codes
+
+// 3 Views code          BSSR(backend server-side rendering)-Bakendda frontendni render qilib olish ya'ni serverda html ni qurib olib uni clientga yuborish. Bu usul traditional usul hisoblanad. 
+// VA Zamonaviy usul SPA(single page application) usuli Buni Reactda qilamiz;
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+
+// 4 Routing codes
+
+app.get("/author", function(req, res){
+  res.render("author");
+})
+
+
+app.post("/create-item", function(req, res){
+  console.log(req.body);
+  res.json({test:"success"});
+})
+
+app.get("/", function(req, res){
+    res.render("harid");
+})
+
+module.exports = app;
+
+
+// app.get("/hello", function(req, res){
+//   res.end("HELLO WORLD HERE");
+// });
+
+// app.get("/gift", function(req, res){
+//   res.end("Siz sovgalar sahifasidasiz!");
+// });
+
+
+/* 
+
+Loyihalarda 2 branch bo'ladi githubda: Master va develop branch bo'ladi. Biz asosan develop branchda ishlab keyin master branchga merge qilamiz. Lekin bu loyihada primitive loyiha bo'lgani uchun biz master branchda ishlaymiz.
+
+*/
--- server.js
+++ server.js
@@ -1,62 +1,25 @@
-// EJS bizga backend da frontendni qurib olis uchun yordam beradi. 
+const http = require("http");
 
+const mongodb = require("mongodb");
 
-console.log('Web serverni boshlash');
-const express = require('express'); // Express bizga backend serverni qurib beradi faqat.
-const app = express(); // bu yerda express bizga object return qiladi.
-const http = require('http');
+let db;
+const connectionString = "mongodb+srv://kuvondik9595:ejFwleu2iK3k0Ndt@clustermit24.akowc.mongodb.net/Reja"
 
-// 1 Intro codes
-// app.use(express.static("public"));  // public folderni clientlarga ochib berish(html, css va images)
-app.use(express.static('./public'));
-app.use(express.json());            // REST API. brauzerdan kelayotgan json formatdagi ma'lumotni object ko'rinishiga o'tqazib beradi
-app.use(express.urlencoded({extended: true})); // TRADITIONAL API. form dan post bo'lib kelgan narsalarni expressda qabul qilish uchun yoziladi
+mongodb.connect(connectionString, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true}, 
+  (err, client) => {
+    if(err) console.log("ERROR on connection MongoDB");
+    else {
+      console.log("MongoDB connection succeed!");
+      module.exports = client;
 
-
-// 2 Session codes
-
-// 3 Views code          BSSR(backend server-side rendering)-Bakendda frontendni render qilib olish ya'ni serverda html ni qurib olib uni clientga yuborish. Bu usul traditional usul hisoblanad. 
-// VA Zamonaviy usul SPA(single page application) usuli Buni Reactda qilamiz;
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-
-// 4 Routing codes
-
-app.get("/author", function(req, res){
-  res.render("author");
-})
-
-
-app.post("/create-item", function(req, res){
-  console.log(req.body);
-  res.json({test:"success"});
-})
-
-app.get("/", function(req, res){
-    res.render("harid");
-})
-
-
-// app.get("/hello", function(req, res){
-//   res.end("HELLO WORLD HERE");
-// });
-
-// app.get("/gift", function(req, res){
-//   res.end("Siz sovgalar sahifasidasiz!");
-// });
-
-
-
-const server = http.createServer(app);
-let port = 3000;
-server.listen(port, function(){
-  console.log(`Server is running succesfully on port ${port}, http://localhost:${port} `)
+      const app = require("./app");
+      const server = http.createServer(app);
+      let port = 3000;
+      server.listen(port, function(){
+        console.log(`Server is running succesfully on port ${port}, http://localhost:${port} `)
+      });
+    }
 });
 
-
-/* 
-
-Loyihalarda 2 branch bo'ladi githubda: Master va develop branch bo'ladi. Biz asosan develop branchda ishlab keyin master branchga merge qilamiz. Lekin bu loyihada primitive loyiha bo'lgani uchun biz master branchda ishlaymiz.
-
-*/
